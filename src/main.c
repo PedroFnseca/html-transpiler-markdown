2dd510036d146c9025b96e805cc557df4a95b8d6
pedrofnseca <pedropereira.pp264@gmail.com>
2024-10-19 19:12:34 -0300

    ref: file handling and implement stack usage in main.c

--- src/main.c
+++ src/main.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "headers/file.h"
+#include "headers/stack.h"
 
 int main() {
-  struct File file = {
-    .content = NULL,
-    .path = "input/index.html"
+  struct File html = {
+    .content = create_stack(),
+    .path = "data/index.html"
   };
 
-  if (read_file(file)) {
+  if (read_file(html)) {
     return 1;
   }
+
+  print_stack(html.content);
+
+  struct File markdown = {
+    .content = create_stack(),
+    .path = "data/index.md"
+  };
+
+  write_file(markdown, html.content);
+
+  free_stack(html.content);
 
   return 0;
 }
